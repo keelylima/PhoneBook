8481e002247b04b179b95fbdf48ddbc9a366cb0a
Kelly Christina <keely06@live.com>
2019-09-27 11:21:40 -0300

    separate files

---
+++ aula02/js/controllers/listaTelefonicaController.js
@@ -0,0 +1,63 @@
+ // O $scope seria o garçom, medicação entre a view e o controller
+ angular.module("listaTelefonica").controller("listaTelefonicaController", ($scope) => {
+
+    //Binding, pega a informação daqui, do controller/$scope e joga pra view
+    $scope.message = 'Lista Telefonica';
+    $scope.contatos = [
+        {
+            nome: 'Kelly',
+            telefone: '(11) 97952-4567'
+        },
+        {
+            nome: 'Thalita',
+            telefone: '(31) 97865-9876'
+        },
+        {
+            nome: 'Claudia',
+            telefone: '(12) 97654-3423'
+        }
+    ];
+    $scope.operadoras = [
+        {
+            nome: 'Oi',
+            codigo: 14,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Vivo',
+            codigo: 15,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Tim',
+            codigo: 41,
+            categoria: 'Celular'
+        },
+    ];
+
+    //O ngModel, ele vai pegar da view (input) e jogar aqui pro controller/$scope
+    $scope.adicionarContato = (contato) => {
+
+        //angular.copy é para criar uma cópia, após adicionar na lista, quando mexo nos inputs não altera o valor
+        $scope.contatos.push(angular.copy(contato));
+
+        //delete é para deletar o objeto após a execução.
+        //limpa os input
+        delete $scope.contato;
+        $scope.contatoForm.$setPristine();
+    }
+
+    $scope.deletarContato = (contatos) => {
+        $scope.contatos = contatos.filter((contato) => {
+            if (!contato.selecionado) {
+                return contato;
+            }
+        })
+    }
+
+    $scope.isContatoSelecionado = (contatos) => {
+        return contatos.some((contato) => {
+            return contato.selecionado;
+        })
+    }
+})
